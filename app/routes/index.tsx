a5a0c85d3cb8ba3d2229a542c7bf56213e3dd675
coji <coji@techtalk.jp>
2022-10-13 16:18:56 +0900

    update

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,12 +1,24 @@
-import { Box, Container, Heading } from "@chakra-ui/react"
+import { useState } from "react"
+import { Box, Container, Heading, Stack, Button } from "@chakra-ui/react"
 import TrendGraph from "~/features/graphs/components/TrendGraph"
 
 export default function Index() {
+  const [count, setCount] = useState(0)
+
   return (
     <Box>
       <Container>
-        <Heading>Remix dashboard example</Heading>
-        <TrendGraph></TrendGraph>
+        <Stack>
+          <Heading>Remix dashboard example</Heading>
+
+          <TrendGraph></TrendGraph>
+          <Button
+            width="full"
+            onClick={() => setCount((current) => current + 1)}
+          >
+            Update
+          </Button>
+        </Stack>
       </Container>
     </Box>
   )
